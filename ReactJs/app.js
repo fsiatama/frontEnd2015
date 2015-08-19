b629af6644ba3324f3eb716674e901d0faccaf3e
Fabian Siatama <fas0980@gmail.com>
2015-08-19 14:09:55 -0500

    avances clase de ReactJs

---
+++ ReactJs/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokeTable from 'components/PokeTable';
+
+var pokemons = [
+	{ number: 1, name: 'Bulbasaur'},
+	{ number: 2, name: 'IvySaur'},
+	{ number: 3, name: 'Venusaur'}
+];
+
+React.render(
+	<PokeTable pokemons={pokemons} />, 
+	document.getElementById('container')
+);
